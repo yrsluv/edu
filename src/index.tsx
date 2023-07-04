06dc9e697947ea8e4dd7d737becfb8c3d727c726
yrslv <yrslvvvv@gmail.com>
2023-07-04 09:49:14 +0300

    add react & scss loader

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import {render} from "react-dom";
+import {Counter} from "./Components/Counter";
+
+
+render(
+    <Counter />,
+    document.getElementById('root')
+)
+console.log(111)
--- src/test.ts
+++
@@ -1,3 +0,0 @@
-export function  someFn(arg: number) {
-    console.log('smthn');
-}
