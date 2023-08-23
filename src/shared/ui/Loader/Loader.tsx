89868130d06b02159e3f0d209c080e553b41cb9d
yrslv <yrslvvvv@gmail.com>
2023-08-23 15:25:24 +0300

    add not_found page, page loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import './Loader.scss';
+
+interface LoaderProps {
+    className?: string;
+}
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames('loader', {}, [className])}>
+        <div className="lds-ellipsis">
+            <div />
+            <div />
+            <div />
+            <div />
+        </div>
+    </div>
+
+);
